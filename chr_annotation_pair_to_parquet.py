bb85ad79f67ce7c94a4fad92b3310106687f84f9
Otto Jolanki <ojolanki@stanford.edu>
2024-03-13 11:25:39 -0700

    calculate a unique key column

--- chr_annotation_pair_to_parquet.py
+++ chr_annotation_pair_to_parquet.py
@@ -1,4 +1,6 @@
 import sys
+from hashlib import sha256
+
 from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
@@ -9,6 +11,12 @@ from pyspark.sql.types import StringType
 
 def parse_base_pairs_from_uniq_id(uniq_id):
     return ':'.join(uniq_id.split(':')[1:3])
+
+def generate_unique_key(*args):
+    concatenated_value = ''.join(args) + 'GRCh38'
+    return sha256(concatenated_value.encode()).hexdigest()
+
+generate_unique_key_udf = udf(generate_unique_key, StringType())
 
 # Get command-line arguments
 args = getResolvedOptions(sys.argv,
@@ -29,6 +37,9 @@ args = getResolvedOptions(sys.argv,
 # Initialize GlueContext and SparkSession
 sc = SparkContext()
 glueContext = GlueContext(sc)
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
 spark = glueContext.spark_session
 
 process_uniq_id_udf = udf(parse_base_pairs_from_uniq_id, StringType())
@@ -46,5 +57,9 @@ parsed_lds = parsed_lds.drop('Uniq_ID_1', 'Uniq_ID_2', '+/-corr').withColumn('R2
 
 parsed_lds = parsed_lds.withColumn('label', lit(args['label'])).withColumn('source', lit(args['source'])).withColumn('source_url', lit(args['source_url'])).withColumn('chr', lit(args['chr'])).withColumn('ancestry', lit(args['ancestry']))
 
+parsed_lds = parsed_lds.withColumn('key', generate_unique_id_udf(col('ancestry'), col('chr'), col('variant_1_base_pair'), col('variant_2_base_pair'))) 
+
 # Write output as parquet to S3
 parsed_lds.write.mode(args['write_mode']).parquet(args['output_path'])
+
+job.commit()
